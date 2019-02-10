f57fd94fa63ba98a11f11d3cbdbe84a85fb9d910
Jonathon Racz <jdracz@umich.edu>
2019-02-10 13:55:14 -0800

    Attempt to fix windows build

--- build.sh
+++ build.sh
@@ -1,10 +1,15 @@
 cd ffmpeg
 
 # Line breaks represent breaks between sections in ./configure.
-./configure \
+CONFIGURE_ARGS="\
     --disable-static --enable-shared \
     --disable-programs \
     --disable-doc --disable-htmlpages --disable-manpages --disable-podpages --disable-txtpages \
-    --disable-avdevice --disable-swresample --disable-swscale --disable-postproc --disable-avfilter
+    --disable-avdevice --disable-swresample --disable-swscale --disable-postproc --disable-avfilter"
 
+if [ ${OSTYPE//[0-9.-]*/} == "msys" ]; then
+    WINDOWS_CONFIGURE_ARGS="--host-os=mingw64"
+fi
+
+./configure $CONFIGURE_ARGS $WINDOWS_CONFIGURE_ARGS
 make
