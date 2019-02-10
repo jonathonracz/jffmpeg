16bd621caf1a5e5fd3e786f3d303306e46b18d92
Jonathon Racz <jdracz@umich.edu>
2019-02-10 14:53:32 -0800

    Attempt to use mingw64_shell.bat on Windows is msys environment is detected

--- build.sh
+++ build.sh
@@ -1,16 +1,16 @@
 cd ffmpeg
 
 echo "$PATH"
+if [ ${OSTYPE//[0-9.-]*/} == "msys" ]; then
+    mingw64_shell.bat
+fi
+
 # Line breaks represent breaks between sections in ./configure.
 CONFIGURE_ARGS="\
-    --disable-static --enable-shared \
-    --disable-programs \
-    --disable-doc --disable-htmlpages --disable-manpages --disable-podpages --disable-txtpages \
-    --disable-avdevice --disable-swresample --disable-swscale --disable-postproc --disable-avfilter"
-
-if [ ${OSTYPE//[0-9.-]*/} == "msys" ]; then
-    msystem.bat MINGW64
-fi
+--disable-static --enable-shared \
+--disable-programs \
+--disable-doc --disable-htmlpages --disable-manpages --disable-podpages --disable-txtpages \
+--disable-avdevice --disable-swresample --disable-swscale --disable-postproc --disable-avfilter"
 
 ./configure $CONFIGURE_ARGS
 make
